c13682d6904099c709a4fcc9e55823b878a51980
CharanTeja <rcharanteja2006@gmail.com>
2025-06-29 17:06:54 +0530

    modified image display using variable

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export const MovieCard = ({ movie : 
     { title, vote_average, poster_path, release_date, original_language }
 }) => {
+    const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
   return (
     <div className='movie-card'>
-        <img src={poster_path ? `https:/image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title}/>
+        <img src={poster_path ? `${IMAGE_BASE_URL}${poster_path}` : '/no-movie.png'} alt={title}/>
         
 
         <div className='mt-4'>
